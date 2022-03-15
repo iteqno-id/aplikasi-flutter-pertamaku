56fc7048639652aec09a3f3bcfea9622aab8b77f
Khofidin <offiedz@gmail.com>
2022-03-16 06:53:33 +0700

    Add icon launcher

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
